8506a8df9233f6c01bfb30eb3f6b1f84e3060870
Jerry Seyi <jerryseyi68@gmail.com>
2023-04-05 11:36:02 +0100

    setting guzzle connect instance:

--- app/services/rapyd/RapydGateway.php
+++ app/services/rapyd/RapydGateway.php
@@ -2,6 +2,9 @@
 
 namespace App\services\rapyd;
 
+use Config;
+use GuzzleHttp\Client;
+
 class RapydGateway
 {
     /**
@@ -24,6 +27,21 @@ class RapydGateway
      * @var mixed
      */
     protected mixed $response;
+
+    /**
+     * Client instance.
+     *
+     * @var Client
+     */
+    protected Client $client;
+
+    public function __construct()
+    {
+        $this->baseUrl = Config::get('rapyd.rapyd_base_url');
+        $this->secretKey = Config::get('rapyd.rapyd_secret_key');
+
+        $this->client = new Client([]);
+    }
 
 
 }
